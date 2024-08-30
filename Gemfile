ffbfcd2b22094afbcc9dc419b71b5135f434475a
snith nhotprasa <lesnith@gmail.com>
2024-08-30 14:52:48 +0200

    add bootstrap, devise

--- Gemfile
+++ Gemfile
@@ -26,6 +26,12 @@ gem "stimulus-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 
+gem "devise"
+
+gem "bootstrap", "~> 5.1.3"
+
+gem "simple_form"
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", ">= 4.0.1"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,4 +9,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :games do
+    resources :parties, only: %i[new create index show] do
+      resources :solutions, only: %i[new create index show]
+    end
+  end
 end
