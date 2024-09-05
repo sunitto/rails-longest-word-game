6dbbad6897df6d8146cb5038c2fedf80cbec2465
snith nhotprasa <lesnith@gmail.com>
2024-09-05 19:25:37 +0200

    rajout de new party et show de party

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,17 +1,23 @@
 class PartiesController < ApplicationController
   def new
     @game = Game.find(params[:game_id])
-    @party = Party.new
+    @party = @game.parties.new
   end
 
   def create
     @game = Game.find(params[:game_id])
-    @party = Party.new(party_params)
+    @party = @game.parties.new(party_params)
     @party.game = @game
     if @party.save
       redirect_to game_party_path(@game, @party)
     else
       render :new
     end
+  end
+
+  private
+
+  def party_params
+    params.require(:party).permit(:word)
   end
 end
